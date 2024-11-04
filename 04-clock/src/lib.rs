7a9f2e5a88b09312ba8d39c21cea76184e5bb6ed
asatpathy314 <asatpathy314@gmail.com>
2024-11-03 23:23:03 -0500

    fixed formatting issues and bug with add_minutes

--- 04-clock/src/lib.rs
+++ 04-clock/src/lib.rs
@@ -1,34 +1,37 @@
 use std::fmt;
 
+#[derive(Debug)]
+#[derive(PartialEq)]
 pub struct Clock {
     hours: i32,
     minutes: i32,
 }
 
 impl Clock {
-    pub fn new(mut hours: i32, mut minutes: i32) -> Clock {
-        let temp_minutes = minutes;
-        minutes = minutes % 60;
-        hours = (hours + temp_minutes / 60) % 24;
-        Self {
-            hours,
-            minutes,
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let raw_hours = (hours + minutes / 60) % 24;
+        let raw_minutes = minutes % 60;
+        Clock {
+            hours: if raw_hours < 0 {24 + raw_hours} else {raw_hours},
+            minutes: if raw_minutes % 60 < 0 {60 + raw_minutes} else {raw_minutes},
         }
     }
 
-    pub fn add_minutes(&mut self, minutes: i32) {
-        let temp_minutes: i32 = self.minutes + minutes;
+    pub fn add_minutes(&mut self, minutes: i32) -> Self {
+        let temp_minutes = self.minutes + minutes;
         self.minutes = temp_minutes % 60;
-        self.hours = self.hours + minutes / 60;
-    }
+        self.hours = self.hours + temp_minutes / 60;
 
-    pub fn to_string(&mut self) -> String {
-        format!("{:0>2}", self)
+        Clock {
+            hours: (self.hours + self.minutes / 60) % 24,
+            minutes: self.minutes % 60,
+        }
     }
 }
 
+
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}:{}", self.hours, self.minutes)
+        write!(f, "{:>02}:{:>02}", self.hours, self.minutes)
     }
 }
