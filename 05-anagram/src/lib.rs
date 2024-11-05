5b763eefdfa77c19cfa9fec3e20666cbf55f6ec1
asatpathy314 <asatpathy314@gmail.com>
2024-11-05 01:29:11 -0500

    refactored code to be more readable

--- 05-anagram/src/lib.rs
+++ 05-anagram/src/lib.rs
@@ -4,35 +4,27 @@ use unicode_segmentation::UnicodeSegmentation;
 use std::collections::HashSet;
 use std::collections::HashMap;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) ->  HashSet<&'a str> {
-    let mut anagram_set: HashSet<&'a str> = HashSet::new();
-    let mut word_freq_map = HashMap::new(); 
-
-    let lowercase_word = word.to_lowercase();
-
-    let grapheme_word_vec= UnicodeSegmentation::graphemes(lowercase_word.as_str(), true).collect::<Vec<&str>>();
-
-    for grapheme in grapheme_word_vec.iter() {
-        *word_freq_map.entry(grapheme).or_insert(0) += 1;
-    }
-
-    for possible_anagram in possible_anagrams.iter() {
-        let mut anagram_freq_map = HashMap::new(); 
-        let lowercase_anagram= possible_anagram.to_lowercase();
-        let grapheme_anagram_vec= UnicodeSegmentation::graphemes(lowercase_anagram.as_str(), true).collect::<Vec<&str>>();
-
-        for grapheme in grapheme_anagram_vec.iter() {
-            *anagram_freq_map.entry(grapheme).or_insert(0) += 1;
-        }
-
-        println!("anagram-map: {:?}", anagram_freq_map);
-        println!("word-map: {:?}", word_freq_map);
-
-        if word_freq_map == anagram_freq_map && lowercase_word != lowercase_anagram{
-            println!("added");
-            anagram_set.insert(possible_anagram);
-        }
-    }
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) ->  HashSet<&'a str> {
+    let anagram_set: HashSet<&'a str> = possible_anagrams.iter()
+                                                             .filter(|anagram| is_anagram(word.to_lowercase(), anagram.to_lowercase()))
+                                                             .map(|anagram| *anagram)
+                                                             .collect();
 
     anagram_set
+}
+
+fn is_anagram(word: String, possible_anagram: String) -> bool {
+    if word == possible_anagram {return false;}
+    get_char_count(&word) == get_char_count(&possible_anagram)
+}
+
+fn get_char_count<'a>(word: &str) -> HashMap<String, i32> {
+    let mut word_freq_map = HashMap::new(); 
+    let word_vec: Vec<&str> = UnicodeSegmentation::graphemes(word, true).collect();
+
+    for grapheme in word_vec.iter() {
+        *word_freq_map.entry(grapheme.to_string()).or_insert(0) += 1;
+    }
+
+    word_freq_map
 }
