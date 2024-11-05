7ceb12000079919910cdbebf631fa74b8fa40bf7
asatpathy314 <asatpathy314@gmail.com>
2024-11-05 01:32:17 -0500

    removed unnecessary extern and followed clippy's suggestions to remove unnecessary code

--- 05-anagram/src/lib.rs
+++ 05-anagram/src/lib.rs
@@ -1,5 +1,3 @@
-extern crate unicode_segmentation;
-
 use unicode_segmentation::UnicodeSegmentation;
 use std::collections::HashSet;
 use std::collections::HashMap;
@@ -7,7 +5,7 @@ use std::collections::HashMap;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) ->  HashSet<&'a str> {
     let anagram_set: HashSet<&'a str> = possible_anagrams.iter()
                                                              .filter(|anagram| is_anagram(word.to_lowercase(), anagram.to_lowercase()))
-                                                             .map(|anagram| *anagram)
+                                                             .copied()
                                                              .collect();
 
     anagram_set
@@ -18,7 +16,7 @@ fn is_anagram(word: String, possible_anagram: String) -> bool {
     get_char_count(&word) == get_char_count(&possible_anagram)
 }
 
-fn get_char_count<'a>(word: &str) -> HashMap<String, i32> {
+fn get_char_count(word: &str) -> HashMap<String, i32> {
     let mut word_freq_map = HashMap::new(); 
     let word_vec: Vec<&str> = UnicodeSegmentation::graphemes(word, true).collect();
 
