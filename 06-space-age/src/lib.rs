dbec251e2aa435ae82b1c90a594ecb4f261fd30c
asatpathy314 <asatpathy314@gmail.com>
2024-11-06 01:10:45 -0500

    initial draft using macros

--- 06-space-age/src/lib.rs
+++ 06-space-age/src/lib.rs
@@ -1,35 +1,50 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+const EARTH_SECONDS: u64 = 31_557_600;
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    s: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+        Duration { s }
     }
 }
 
 pub trait Planet {
+    fn earth_years() -> f64;
+
     fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+        (d.s as f64 / EARTH_SECONDS as f64) / Self::earth_years()
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! decl_Planet {
+    ( $( $x: ident ), * ) => {
+        $(
+            pub struct $x;
+        )*
+    }
+}
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+macro_rules! impl_Planet {
+    ( $( ($x:ident, $y:literal)  ),*) => {
+        $(impl Planet for $x {
+            fn earth_years() -> f64 {
+                $y
+            }
+        })*
+    }
+}
+
+decl_Planet!(Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune);
+impl_Planet!(
+    (Mercury, 0.2408467),
+    (Venus, 0.61519726),
+    (Earth, 1.0),
+    (Mars, 1.8808158),
+    (Jupiter, 11.862615),
+    (Saturn, 29.447498),
+    (Uranus, 84.016846),
+    (Neptune, 164.79132)
+);
