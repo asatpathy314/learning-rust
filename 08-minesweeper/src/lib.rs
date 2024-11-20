03374d61a7c1f4bba9f02577f94cddef37c8a692
asatpathy314 <asatpathy314@gmail.com>
2024-11-20 00:06:45 -0500

    restarted minesweeper exercise

--- 08-minesweeper/src/lib.rs
+++ 08-minesweeper/src/lib.rs
@@ -1,3 +1,27 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+    let size = minefield.len();
+    let mut board: Vec<Vec<i32>> = vec![vec![0; size]; size];
+
+    for (i, &str) in minefield.iter().enumerate() {
+        let str_bytes = str.as_bytes();
+        for (j, char) in str_bytes.iter().enumerate() {
+            if *char == b'*' {
+                board[i][j] = -1;
+            }
+        }
+    }
+
+    for (row_ind, row) in board.iter().enumerate() {
+        for (col_ind, value) in row.iter().enumerate() {
+            if *value == -1 {
+                update_values(board, row_ind, col_ind);
+            }
+        }
+    }
+
+    Vec::with_capacity(10)
+}
+
+fn update_values(board: Vec<Vec<i32>>, row: usize, col: usize) -> () {
+    
 }
