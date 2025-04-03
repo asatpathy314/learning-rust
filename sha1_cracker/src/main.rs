e4a4b0c819a5a1febefca6c82e8994da58ab7f50
asatpathy314 <asatpathy314@gmail.com>
2025-04-02 22:19:34 -0400

    initial code for sha1 cracker

---
+++ sha1_cracker/src/main.rs
@@ -0,0 +1,23 @@
+use std::{
+    env,
+    error:Error,
+};
+
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
+        Ok(())
+    }
+
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());       
+    }
+
+    Ok(())
+}
