9032875719655746f8f000cd3747106add60633e
asatpathy314 <asatpathy314@gmail.com>
2025-04-23 01:05:36 -0400

    update: trying minesweeper again, feeling better after a few more months of rust

--- 08-minesweeper/src/lib.rs
+++ 08-minesweeper/src/lib.rs
@@ -1,14 +1,39 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let mut minefield_flattened: Vec<u8> = Vec::new();
-    for row in minefield.iter() {
-        for char in row.as_bytes() {
-            minefield_flattened.push(*char)
+    let m: usize = minefield.len();
+    let n: usize = if m > 0 { minefield[0].len() } else { 0 };
+
+    let minefield: Vec<Vec<u8>> = minefield.into_iter()
+        .map(|&row| {
+            row.as_bytes().to_vec()
+        })
+        .collect();
+    
+    for (i: i32, row) in minefield.iter().enumerate() {
+        for (j: i32, col) in row.iter().enumerate() {
+            if *col == b'*' {
+
+                let neighbors = get_neighbors(i, j, m, n);
+            }
         }
     }
-    println!("{:?}", minefield_flattened);
-    Vec::new()
+    
+
+
+    vec![String::from("not implemented")]
 }
 
-pub fn main() {
-    annotate(&[" * ", "   ", " * "]);
+fn get_neighbors(i: i32, j: i32, m: usize, n: usize) -> Vec<(i32, i32)> {
+    let mut neighbors: Vec<(i32, i32)> = vec![]; 
+    for x in -1..2 {
+        for y in -1..2 {
+            if is_in_bounds(i + x, j + y, m, n) && (x != 0 && y != 0) {
+                neighbors.push((i + x, j + y));
+            }
+        }
+    }
+    neighbors
+}
+
+fn is_in_bounds(i: i32, j: i32, m: usize, n: usize) -> bool {
+    i > -1 && i < m.try_into().unwrap() && j > -1 && j < n.try_into().unwrap()
 }
