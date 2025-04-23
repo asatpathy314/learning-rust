d2ce5eb9d5b11484a22c8c7c8172315cad13cf59
asatpathy314 <asatpathy314@gmail.com>
2025-04-23 00:35:57 -0400

    starting to make dns_resolver async

--- black-hat-rust/tricoder/src/main.rs
+++ black-hat-rust/tricoder/src/main.rs
@@ -1,6 +1,9 @@
-use rayon::prelude::*;
-use reqwest::{blocking::Client, redirect};
-use std::{env, time::Duration};
+use futures::{stream, StreamExt};
+use reqwest::Client;
+use std::{
+    env,
+    time::{Duration, Instant},
+};
 
 mod error;
 pub use error::Error;
@@ -10,42 +13,25 @@ mod subdomains;
 use model::Subdomain;
 mod common_ports;
 
-fn main() -> Result<(), anyhow::Error> {
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        return Err(Error::Usage.into());
+        return Err(Error::CliUsage.into());
     }
 
     let target = args[1].as_str();
 
-    let http_timeout = Duration::from_secs(5);
-    let http_client = Client::builder()
-        .redirect(redirect::Policy::limited(4))
-        .timeout(http_timeout)
-        .build()?;
+    let http_timeout = Duration::from_secs(10);
+    let http_client = Client::builder().timeout(http_timeout).build()?;
 
-    let pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(256)
-        .build()
-        .unwrap();
+    let ports_concurrency = 200;
+    let subdomains_concurrency = 100;
+    let scan_start = Instant::now();
 
-    pool.install(|| {
-        let scan_result: Vec<Subdomain> = subdomains::enumerate(&http_client, target)
-            .unwrap()
-            .into_par_iter()
-            .map(ports::scan_ports)
-            .collect();
-
-        for subdomain in scan_result {
-            println!("{}:", &subdomain.domain);
-            for port in &subdomain.open_ports {
-                println!("    {}", port.port);
-            }
-
-            println!("");
-        }
-    });
+    let subdomains = subdomains::enumerate(&http_client, target).await?;
+    
 
     Ok(())
 }
